5b7c596f638879b165bcd8a4c78f0237168d3129
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-02-20 17:19:32 +0530

    Time: 113 ms (56.00%) | Memory: 19.1 MB (65.05%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,5 +1,16 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        c=1
         for i in range(len(nums)):
-            if nums.count(nums[i])==1:
+            if len(nums)==1:
+                return nums[0]
+            elif nums[i]==nums[i+1]:
+                c+=1
+            elif nums[i]!=nums[i+1] and c==1:
                 return nums[i]
+            elif nums[i]!=nums[i+1] and c!=1:
+                c=1
+                if nums[i+1]==nums[-1]:
+                    return nums[i+1]
+                
