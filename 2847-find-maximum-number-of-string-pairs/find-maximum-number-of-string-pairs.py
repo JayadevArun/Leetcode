97db26334d82f143fabc88fb8e7c33f430700e8f
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-03-02 22:45:45 +0530

    Time: 49 ms (59.75%) | Memory: 16.6 MB (62.98%) - LeetSync

--- 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.py
+++ 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.py
@@ -1,8 +1,19 @@
 class Solution:
     def maximumNumberOfStringPairs(self, words: List[str]) -> int:
         c=0
-        for i in range (len(words)):
-            for j in range(i+1,len(words)):
-                if words[i]==words[j][::-1]:
-                    c+=1
+        hm={}
+        for i in words:
+            rev=i[::-1]
+            if rev in hm:
+                c+=1
+                del hm[rev]
+            else:
+                hm[i]=1
         return c
+
+        # c=0
+        # for i in range (len(words)):
+        #     for j in range(i+1,len(words)):
+        #         if words[i]==words[j][::-1]:
+        #             c+=1
+        # return c
