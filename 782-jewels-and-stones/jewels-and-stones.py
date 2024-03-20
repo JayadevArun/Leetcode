27c528d9a30333a815ba92f7ed0bd5c3eee8c16d
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-03-20 22:11:01 +0530

    Time: 35 ms (60.21%) | Memory: 16.6 MB (43.94%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -1,7 +1,16 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         c=0
-        for i in stones:
-            if i in jewels:
+        hm={}
+        for i in jewels:
+            hm[i]=1
+        for j in stones:
+            if j in hm:
                 c+=1
         return c
+
+        # c=0
+        # for i in stones:
+        #     if i in jewels:
+        #         c+=1
+        # return c
