8b7189c5f0291d02d16129a99a1284a87cfe2126
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-04-15 23:37:39 +0530

    Time: 68 ms (6.54%) | Memory: 16.7 MB (15.81%) - LeetSync

--- 1547-destination-city/destination-city.py
+++ 1547-destination-city/destination-city.py
@@ -1,10 +1,19 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        hm1={}
-        hm2={}
+        arr1 = []
+        arr2 = []
         for path in paths:
-            hm1[path[0]]=path[1]
-            hm2[path[1]]=1
-        for item in hm2.keys():
-            if item not in hm1:
+            arr1.append(path[0])
+            arr2.append(path[1])
+        for item in arr2:
+            if item not in arr1:
                 return item
+        
+        # hm1={}
+        # hm2={}
+        # for path in paths:
+        #     hm1[path[0]]=path[1]
+        #     hm2[path[1]]=1
+        # for item in hm2.keys():
+        #     if item not in hm1:
+        #         return item
