b65b72ff79efc7cb53d5af7ac679f2f67e489ccd
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-04-15 23:01:59 +0530

    Time: 104 ms (62.53%) | Memory: 17.3 MB (8.83%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,12 +1,18 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         hm={}
-        L=[]
         for i in range(len(heights)):
-            if heights[i] not in hm:
-                hm[heights[i]]=names[i]
-        l=list(hm.keys())
-        l.sort(reverse=True)
-        for height in l:
-            L.append(hm[height])
-        return L
+            hm[heights[i]]=names[i]
+        hm=dict(sorted(hm.items(),reverse=True))
+        return hm.values()
+        
+        # hm={}
+        # L=[]
+        # for i in range(len(heights)):
+        #     if heights[i] not in hm:
+        #         hm[heights[i]]=names[i]
+        # l=list(hm.keys())
+        # l.sort(reverse=True)
+        # for height in l:
+        #     L.append(hm[height])
+        # return L
