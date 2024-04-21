6f0c5ba639ecb9cca9c65744da9d264efd214479
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-04-21 15:23:03 +0530

    Time: 96 ms (97.70%) | Memory: 23.5 MB (74.22%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.py
+++ 975-range-sum-of-bst/range-sum-of-bst.py
@@ -10,10 +10,18 @@ class Solution:
 
         if not root:
             return 0
-        if low<=root.val<=high:
-            sum=root.val
-        else:
-            sum=0
-        lsum=self.rangeSumBST(root.left,low,high)
-        rsum=self.rangeSumBST(root.right,low,high)
-        return sum+lsum+rsum
+        if root.val<low:
+            return self.rangeSumBST(root.right,low,high)
+        elif root.val>high:
+            return self.rangeSumBST(root.left,low,high)
+        return root.val+self.rangeSumBST(root.right,low,high)+self.rangeSumBST(root.left,low,high)
+
+        # if not root:
+        #     return 0
+        # if low<=root.val<=high:
+        #     sum=root.val
+        # else:
+        #     sum=0
+        # lsum=self.rangeSumBST(root.left,low,high)
+        # rsum=self.rangeSumBST(root.right,low,high)
+        # return sum+lsum+rsum
