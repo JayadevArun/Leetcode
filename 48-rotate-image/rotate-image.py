cd3c5bf140ff27c382187a56696b3d67cac31b72
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-07-02 21:12:28 +0530

    Time: 33 ms (87.41%) | Memory: 16.6 MB (39.05%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -7,4 +7,4 @@ class Solution:
             for c in range(r, len(matrix[0])):
                 matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
         for r in range(len(matrix[0])):
-            matrix[r].reverse()
+             matrix[r]=matrix[r][::-1]
