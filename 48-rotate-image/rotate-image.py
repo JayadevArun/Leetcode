d4d80bf890f8439d7a0f48173b1c74791ad482a8
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-07-02 20:03:56 +0530

    Time: 40 ms (47.95%) | Memory: 16.6 MB (39.05%) - LeetSync

---
+++ 48-rotate-image/rotate-image.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        l=0
+        r=len(matrix[0])-1
+        while l<r:
+            for i in range(r-l):
+                t=l
+                b=r
+                temp=matrix[t][l+i]
+                matrix[t][l+i]=matrix[b-i][l]
+                matrix[b-i][l]=matrix[b][r-i]
+                matrix[b][r-i]=matrix[t+i][r]
+                matrix[t+i][r]=temp
+            l+=1
+            r-=1
