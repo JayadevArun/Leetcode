0c4b668323fffb99f756faa67e8e61c344ff7dee
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-07-03 19:47:01 +0530

    Time: 43 ms (19.12%) | Memory: 16.5 MB (60.22%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,10 +1,16 @@
 class Solution:
+
+    def sumSquareDigits(self, n: int) -> int:
+        sum = 0
+        while n > 0:
+            sum += (n % 10) ** 2
+            n //= 10
+        return sum
+        
     def isHappy(self, n: int) -> bool:
-        hm = {}
-        while n != 1:
-            if n in hm:
-                return False
-            hm[n] = 0
-            n = sum(int(i) ** 2 for i in str(n))
-        return True
+        s = set()
+        while n > 1 and n not in s:
+            s.add(n)
+            n = self.sumSquareDigits(n)
+        return n == 1
             
