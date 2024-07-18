d48279520cff9a8b6b4053231ace90d62b9754d5
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-07-18 22:50:06 +0530

    Time: 58 ms (91.47%) | Memory: 17.2 MB (16.71%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,19 +1,18 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        st=[]
+        st = []
         for item in tokens:
-            if item=='+':
-                st.append(st.pop()+st.pop())
-            elif item=='-':
-                a=st.pop()
-                b=st.pop()
-                st.append(b-a)
-            elif item=='*':
-                st.append(st.pop()*st.pop())
-            elif item=='/':
-                c=st.pop()
-                d=st.pop()
-                st.append(int(d/c))
+            if item in "+-*/":
+                b = st.pop()
+                a = st.pop()
+                if item == '+':
+                    st.append(a + b)
+                elif item == '-':
+                    st.append(a - b)
+                elif item == '*':
+                    st.append(a * b)
+                elif item == '/':
+                    st.append(int(a / b))
             else:
                 st.append(int(item))
         return st[0]
