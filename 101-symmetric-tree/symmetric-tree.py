f5b48c4b27c4c6057d4128618ee710a748ebdcfb
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-07-20 15:22:36 +0530

    Time: 33 ms (80.45%) | Memory: 16.6 MB (11.02%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -6,12 +6,17 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+
         if not root:
             return True
+
         def isSym(l,r):
             if not l and not r:
                 return True
             if not l or not r:
                 return False
-            return l.val==r.val and isSym(l.left,r.right) and isSym(l.right,r.left)
+            if l.val!=r.val:
+                return False
+            return isSym(l.left,r.right) and isSym(l.right,r.left)
+            
         return isSym(root.left,root.right)
