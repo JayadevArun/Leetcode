423d6fe75b5215f83eb9bff2378f1a0110c31ae0
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-07-21 14:36:34 +0530

    Time: 73 ms (73.45%) | Memory: 17.8 MB (23.26%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,21 +1,23 @@
 class Solution:
-    def BS(self,nums,target,leftshift):
-        l,r=0,len(nums)-1
-        i=-1
-        while l<=r:
-            m=(l+r)//2
-            if nums[m]>target:
-                r=m-1
-            elif nums[m]<target:
-                l=m+1
-            else:
-                i=m
-                if leftshift:
-                    r=m-1
-                else:
-                    l=m+1
-        return i
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        l=self.BS(nums,target,True)
-        r=self.BS(nums,target,False)
+
+        def BS(nums,target,leftshift):
+            l,r=0,len(nums)-1
+            i=-1
+            while l<=r:
+                m=(l+r)//2
+                if nums[m]>target:
+                    r=m-1
+                elif nums[m]<target:
+                    l=m+1
+                else:
+                    i=m
+                    if leftshift:
+                        r=m-1
+                    else:
+                        l=m+1
+            return i
+            
+        l=BS(nums,target,True)
+        r=BS(nums,target,False)
         return [l,r]
