898c20bd7ffcb1d7f7ccee539009c2ce8ff99fbc
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-07-28 23:03:09 +0530

    Time: 463 ms (42.16%) | Memory: 19.5 MB (86.18%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
@@ -6,13 +6,9 @@ class Solution:
         for r in range(len(nums)):
             if nums[r]==0:
                 zero+=1
-            else:
-                zero+=0
             while zero>1:
                 if nums[l]==0:
                     zero-=1
-                else:
-                    zero-=0
                 l+=1
             res=max(res,r-l)
         return res
