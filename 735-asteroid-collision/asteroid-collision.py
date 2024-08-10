90f22c732717442a72026b2360783858212fd4c2
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-08-10 10:17:45 +0530

    Time: 85 ms (41.12%) | Memory: 17.8 MB (39.37%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -2,7 +2,7 @@ class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         st=[]
         for ast in asteroids:
-            while st and st[-1]>0 and ast<0:
+            while st and ast<0<st[-1]:
                 dif=ast+st[-1]
                 if dif>0:
                     ast=0
