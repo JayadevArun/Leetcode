8e75da7015c5bbda3da97d315a8e4927f0bdc4bb
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-08-11 16:41:33 +0530

    Time: 47 ms (53.78%) | Memory: 16.7 MB (58.62%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -10,5 +10,5 @@ class Solution:
                 high=mid-1
             elif mid<len(nums)-1 and nums[mid]<nums[mid+1]:
                 low=mid+1
-            else:
+            elif (mid==0 or nums[mid]>nums[mid-1]) and (mid==len(nums)-1 or nums[mid]>nums[mid+1]):
                 return mid
