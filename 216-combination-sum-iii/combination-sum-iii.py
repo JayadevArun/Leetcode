c739159318042e4183cc5dca391d552bc42f3a85
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-08-17 13:02:26 +0530

    Time: 26 ms (97.20%) | Memory: 16.4 MB (65.42%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.py
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -12,10 +12,11 @@ class Solution:
             if len(cur)>k or rem<0:
                 return
 
-            for i in range(low,10):
-                cur.append(i)
-                recback(i+1,cur,rem-i,k)
-                cur.pop()
+            if len(cur)<k and rem>0:
+                for i in range(low,10):
+                    cur.append(i)
+                    recback(i+1,cur,rem-i,k)
+                    cur.pop()
 
         recback(1,[],n,k)
         return res
