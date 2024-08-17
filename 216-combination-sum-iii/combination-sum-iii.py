cff16fd9e7380bbde5978f905a9eb034563e21f7
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-08-17 13:01:17 +0530

    Time: 29 ms (92.03%) | Memory: 16.6 MB (29.26%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+
+        res = []
+
+        def recback(low,cur,rem,k):
+
+            if len(cur)==k and rem==0:
+                res.append(cur[:])
+                return
+
+            if len(cur)>k:
+                return
+
+            if rem<0:
+                return
+
+            for i in range(low,10):
+                cur.append(i)
+                recback(i+1,cur,rem-i,k)
+                cur.pop()
+
+        recback(1,[],n,k)
+        return res
