e55bf39e900f36ffd49ac9476dc034ef0f11a232
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-08-23 00:24:57 +0530

    Time: 60 ms (23.98%) | Memory: 16.7 MB (29.09%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,10 +1,6 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         n=len(cost)
-        if n==0:
-            return 0
-        if n==1:
-            return cost[0]
         a=cost[0]
         b=cost[1]
         for i in range(2,n):
