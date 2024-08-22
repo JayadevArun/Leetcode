a0ec9a37b49286e39cce8dc28d28a6e38ecedb94
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-08-23 00:26:31 +0530

    Time: 50 ms (82.84%) | Memory: 16.6 MB (89.75%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -3,7 +3,7 @@ class Solution:
         a=cost[0]
         b=cost[1]
         for i in range(2,len(cost)):
-            temp=min(a+cost[i],b+cost[i])
+            temp=cost[i]+min(a,b)
             a=b
             b=temp
         return min(a,b)
