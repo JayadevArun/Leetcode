d56065318c3b4a132f31eba58cdf779c052ee031
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-08-25 18:08:53 +0530

    Time: 33 ms (75.79%) | Memory: 16.4 MB (93.25%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,9 +1,13 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        sum1=0
-        sum2=0
-        for num in nums:
-            s=max(sum1+num,sum2)
+    def rob(self, nums: List[int]) -> int: 
+        if not nums:
+            return 0
+        if len(nums)==1:
+            return nums[0]
+        sum1=nums[0]
+        sum2=max(nums[0],nums[1])
+        for i in range(2,len(nums)):
+            s=max(sum1+nums[i],sum2)
             sum1=sum2
             sum2=s
-        return max(sum1,sum2)
+        return sum2
