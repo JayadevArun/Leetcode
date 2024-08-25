d049543150f6e76ff58143e1ea8e3106ea798951
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-08-25 23:30:48 +0530

    Time: 43 ms (27.44%) | Memory: 16.5 MB (73.41%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
@@ -1,5 +1,11 @@
 class Solution:
     def numTilings(self, n: int) -> int:
+        if n==1:
+            return 1
+        if n==2:
+            return 2
+        if n==3:
+            return 5
         dp = [1, 2, 5] + [0] * n
         for i in range(3, n):
             dp[i] = (dp[i-1] * 2 + dp[i-3]) % 1000000007
