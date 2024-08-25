8340bc6f4a047749bc51e8ef9df09f3932ab4a73
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-08-25 23:26:04 +0530

    Time: 34 ms (79.01%) | Memory: 16.5 MB (73.41%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
@@ -8,7 +8,7 @@ class Solution:
         if n==3:
             return 5
         dp=[0]*(n+1)
-        dp[1],dp[2],dp[3]=1,2,5
+        dp[0],dp[1],dp[2],dp[3]=1,1,2,5
         for i in range(4,n+1):
             dp[i]=(2*dp[i-1]+dp[i-3])%mod
         return dp[n]
