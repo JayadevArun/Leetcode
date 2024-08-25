42507f121785b3af698c1acf6c9ffa21543eca6f
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-08-25 18:19:33 +0530

    Time: 33 ms (75.79%) | Memory: 16.6 MB (26.92%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -4,10 +4,9 @@ class Solution:
             return 0
         if len(nums)==1:
             return nums[0]
-        sum1=nums[0]
-        sum2=max(nums[0],nums[1])
+        dp=[0]*len(nums)
+        dp[0]=nums[0]
+        dp[1]=max(nums[0],nums[1])
         for i in range(2,len(nums)):
-            s=max(sum1+nums[i],sum2)
-            sum1=sum2
-            sum2=s
-        return max(sum1,sum2)
+            dp[i]=max(dp[i-2]+nums[i],dp[i-1])
+        return dp[len(nums)-1]
