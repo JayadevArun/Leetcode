6fa4035b6880d4c9bbb14d90e77c58d8c8a2e23d
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-08-29 22:33:49 +0530

    Time: 53 ms (71.88%) | Memory: 18.3 MB (55.41%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
@@ -10,5 +10,6 @@ class Solution:
             return root
         elif val<root.val:
             return self.searchBST(root.left,val)
-        return self.searchBST(root.right,val)
+        else:
+            return self.searchBST(root.right,val)
         
