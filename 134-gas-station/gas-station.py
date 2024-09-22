c5b1fc6fc38ffef07275cdab550d5241aaff513d
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2024-09-22 13:01:24 +0530

    Time: 753 ms (99.38%) | Memory: 22 MB (32.10%) - LeetSync

---
+++ 134-gas-station/gas-station.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas)<sum(cost):
+            return -1
+        dif=0
+        s=0
+        for i in range(len(gas)):
+            dif+=gas[i]-cost[i]
+            if dif<0:
+                dif=0
+                s=i+1
+        return s
+
+                
+
+        
