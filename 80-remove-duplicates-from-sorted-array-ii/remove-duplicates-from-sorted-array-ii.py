72b1eefeacc72feef5621e8b06279073af6e2fa5
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2025-02-09 11:34:45 +0530

    Time: 90 ms (6.16%) | Memory: 20.3 MB (12.11%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -3,14 +3,14 @@ class Solution:
         c=1
         res=1
         for i in range(1,len(nums)):
-            if nums[i]==nums[i-1]:
+            if nums[i]!=nums[i-1]:
+                c=1
+                nums[res]=nums[i]
+                res+=1
+            else:
                 c+=1
                 if c<=2:
                     nums[res]=nums[i]
                     res+=1
-            else:
-                c=1
-                nums[res]=nums[i]
-                res+=1
         return res
             
