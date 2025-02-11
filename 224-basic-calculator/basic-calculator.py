a20841ae2b1ada15a2a81aad661b54e9c3baacdd
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2025-02-11 23:31:16 +0530

    Time: 19 ms (94.85%) | Memory: 18.4 MB (48.38%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.py
@@ -0,0 +1,33 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+
+        st=[]
+        num=0
+        sign=1
+        res=0
+
+        for ch in s:
+            if ch.isdigit():
+                num=10*num+int(ch)
+
+            elif ch in "+-":
+                res+=sign*num
+                num=0
+                if ch=="+":
+                    sign=1
+                else:
+                    sign=-1
+
+            elif ch=="(":
+                st.append(res)
+                st.append(sign)
+                res=0
+                sign=1
+
+            elif ch==")":
+                res+=sign*num
+                res*=st.pop()
+                res+=st.pop()
+                num=0
+
+        return res+sign*num
