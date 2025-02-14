667f755cfbff7b6bdb640cf39892fd654f159155
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2025-02-14 11:57:15 +0530

    Time: 7 ms (98.18%) | Memory: 21.3 MB (40.93%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,5 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        if not strs:
+            return []
         hm=defaultdict(list)
         for word in strs:
             sword=tuple(sorted(word))
