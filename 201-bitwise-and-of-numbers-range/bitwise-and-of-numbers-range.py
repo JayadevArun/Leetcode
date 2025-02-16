a8caf09f86d08dfabf9ad4ad8da62bdd80e4d61f
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2025-02-16 23:39:15 +0530

    Time: 5 ms (49.94%) | Memory: 18 MB (19.55%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
@@ -1,12 +1,9 @@
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
-        if left==0:
-            return 0
-        if left==right:
+        if left==0 or left==right:
             return left
         if len(bin(left))!=len(bin(right)):
             return 0
         while right>left:
             right &=(right-1)
         return right
-        
