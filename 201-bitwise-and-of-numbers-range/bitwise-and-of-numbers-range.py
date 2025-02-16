ebfcb051993b9a3cad5b613e00c05370c0fe78d6
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2025-02-16 23:37:10 +0530

    Time: 5 ms (49.94%) | Memory: 18 MB (19.55%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
@@ -4,6 +4,8 @@ class Solution:
             return 0
         if left==right:
             return left
+        if len(bin(left))!=len(bin(right)):
+            return 0
         while right>left:
             right &=(right-1)
         return right
