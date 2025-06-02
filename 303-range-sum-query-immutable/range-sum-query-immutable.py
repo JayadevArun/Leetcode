2980c9def6da69df8f3ccc14cc212656449c058f
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2025-06-02 11:32:49 +0530

    Time: 3 ms (73.99%) | Memory: 21.8 MB (61.22%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.py
+++ 303-range-sum-query-immutable/range-sum-query-immutable.py
@@ -9,9 +9,10 @@ class NumArray:
 
     def sumRange(self, left: int, right: int) -> int:
         r=self.arr[right]
-        l=self.arr[left-1]
-        if left==0:
-            return r
+        if left!=0:
+            l=self.arr[left-1]
+        else:
+            l=0
         return r-l
 
 
