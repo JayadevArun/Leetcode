54e1bd9125d13fee6cbd09abda0f2edb9a52a4bc
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2025-06-13 21:09:14 +0530

    Time: 7 ms (47.08%) | Memory: 18.8 MB (98.47%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        indeg=[0]*numCourses
+        g=[[] for i in range(numCourses)]
+
+        for a,b in prerequisites:
+            g[b].append(a)
+            indeg[a]+=1
+
+        q=deque()
+        for i in range(numCourses):
+            if indeg[i]==0:
+                q.append(i)
+
+        count=0
+        while q:
+            c=q.popleft()
+            count+=1
+            for n in g[c]:
+                indeg[n]-=1
+                if indeg[n]==0:
+                    q.append(n)
+        
+        if count==numCourses:
+            return True
+        else:
+            return False
