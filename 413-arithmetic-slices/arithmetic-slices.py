0267d37735de3f47364b2ac563735c680c80568d
Jayadev Arun <94240796+JayadevArun@users.noreply.github.com>
2025-08-19 20:43:12 +0530

    Time: 2 ms (19.96%) | Memory: 17.9 MB (95.21%) - LeetSync

---
+++ 413-arithmetic-slices/arithmetic-slices.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        n=len(nums)
+        if n<3:
+            return 0
+
+        count=0
+        left=0
+
+        while left+2<n:
+            diff=nums[left+1]-nums[left]
+            right=left+2
+            while right<n and diff==nums[right]-nums[right-1]:
+                right+=1
+                count+=1
+            left+=1
+        
+        return count
+
+        return count
